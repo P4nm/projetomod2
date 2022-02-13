5f717dd42108b5fac4a1d9ca92fc9eca6690aa42
Panmela <panmelatonassi@hotmail.com>
2022-02-13 00:38:18 -0300

    modificação js e html

--- cadastro.js
+++ cadastro.js
@@ -1,2 +1,25 @@
 const cep = $("#cep")
-console.log (cep)
+const button = $("#button")
+const endereço = $("#endereco")
+const rua = $("#rua")
+const bairro = $("#bairro")
+const estado = $("#estado")
+const cidade = $("#cidade")
+
+button.on("click", function (event) {
+    event.preventDefault()
+    const busca = cep.val() 
+    $.getJSON(`https://viacep.com.br/ws/${busca}/json/`, ( dados ) => {
+        console.log( dados )
+    rua.val (dados.logradouro)
+    bairro.val(dados.bairro)
+    estado.val(dados.uf)
+    cidade.val(dados.localidade)
+
+    })
+
+})
+
+
+
+    
---
+++ recuperacao.js
@@ -0,0 +1,8 @@
+const emailRecupera = $("#email")
+const recuperarSenha = $("#button")
+
+recuperarSenha.on("click", function (event) {
+    event.preventDefault() //  preventDefault ele evite que atualize a pagina
+    alert ("Encaminhamos um e-mail para recuperação de senha.")
+    console.log (emailRecupera.val())
+})
